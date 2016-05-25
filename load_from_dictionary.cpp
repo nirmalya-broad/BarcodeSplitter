85dbd5801bb490467cd2a9fa257195d1c9731c20
Nirmalya Bandyopadhyay <nirmalya@broadinstitute.org>
2016-05-25 16:01:59 -0400

    Clean up of load_from_dictionary

--- load_from_dictionary.cpp
+++ load_from_dictionary.cpp
@@ -1,17 +1,6 @@
 /*
     load_and_search.cpp
 	
-	The code has been modified by Nirmalya Bandyopadhyay from 
-    ------
-	Stephen Holiday 2011
-    http://stephenholiday.com
-    (Exception, Distance Algorithm by Anders Sewerin Johansen)
-
-    The code is under the [Apache 2.0](http://www.apache.org/licenses/LICENSE-2.0) license.
-
-    This is a utility for creating a BKTree from a text file and saving to disk.
-    The input files should have one entry per line.
-    
     ./bkLoad [from] [to]
     ./bkLoad /usr/share/dict/words wordTree.dat
 
@@ -31,43 +20,175 @@
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/regex.hpp>
+#include <boost/program_options.hpp>
 
-int main(int argc, char* argv[]) { 
+namespace po = boost::program_options;
+
+class load_from_dict{
+
+	public:
+	bool parse_args(int argc, char* argv[]);
+	bool build_tree_simple();
+	bool build_tree_complex();
+	void save_tree();
+	std::string& get_type();
+	void print_help();
+	load_from_dict();
+	~load_from_dict();
+
+	private:
+	BKTree<std::string> tree;
+	std::string infile;
+	std::string outfile;
+	std::string ltype;
+    po::options_description desc;
+
+};
+
+
+void load_from_dict::print_help() {
+	std::cout << desc << "\n";
+    std::cout << "usage: bkLoad -i <infile> -o <outfile> -t <type>\n\n";
+}
+
+load_from_dict::load_from_dict() {
+	tree = BKTree<std::string>();
+}
+
+std::string& load_from_dict::get_type() {
+	return ltype;
+}
+
+load_from_dict::~load_from_dict() {
+	// Nothing yet	
+}
+
+bool load_from_dict::build_tree_simple() {
+	std::ifstream words(infile);
+	std::string lstr;
+
+	if (!words.is_open()) {
+    	std::cerr << "The infile cannot be open!\n";
+		return false;
+	} else {
+		while (std::getline(words, lstr)) {
+			tree.insert(lstr);
+		}
+	}
+	std::cout<< "Loaded " <<tree.size()<< " entries" << std::endl;
+	return true;
+}
+
+void load_from_dict::save_tree() {
     
-    BKTree<std::string> tree=BKTree<std::string>();
-    
-    std::ifstream words (argv[1]);
+	std::ofstream ofs(outfile);
+    boost::archive::text_oarchive oa(ofs);
+    oa << tree;
+	ofs.close();
+	return;
+}
+
+
+bool load_from_dict::build_tree_complex() {
+ std::ifstream words (infile);
     std::string str;
     
-    std::ofstream ofs(argv[2]);
     boost::regex expr ("NNNNNN(\\w{6})");
 	boost::smatch what;	
     
-    if ( !words.is_open() ) {
-      // The file could not be opened
+    if (!words.is_open()) {
+    	std::cerr << "The infile cannot be open!\n";
+		return false;
     }
     else {
         while(std::getline(words, str)) {
 				
 			// Extract the six bases from each of the line after NNNNNN
 			bool res = boost::regex_search(str, what, expr);
-			//std::cout << str << "\n";
 			if (res) {				
-				//std::cout << what[1] << "\n";
 				std::string lstr = what[1];
         		tree.insert(lstr);
 			}
-			//std::cout << res << "\n";
-			
 		}
     }
     
     std::cout<<"Loaded "<<tree.size()<< " entries"<<std::endl;
-    
-    {
-        boost::archive::text_oarchive oa(ofs);
-        oa<<tree;
+	return true;
+}
+
+bool load_from_dict::parse_args(int argc, char* argv[]) {
+	 bool all_set = true;
+
+    desc.add_options()
+        ("help,h", "produce help mesage")
+        ("infile,i", po::value<std::string>(&infile), "Input file")
+        ("outfile,o", po::value<std::string>(&outfile), "Output file")
+		("type,t", po::value<std::string>(&ltype), "Input file type")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        return 0;
     }
+
+    if (!vm.count("infile")) {
+        std::cout << "Infile not set.\n";
+        all_set = false;
+    } else {
+        std::cout << "Infile is set to: " << infile << ".\n";
+    }
+
+    if (!vm.count("outfile")) {
+        std::cout << "Outfile not set.\n";
+        all_set = false;
+    } else {
+        std::cout << "Outfile is set to: " << outfile << ".\n";
+    }
+
+	if (!vm.count("type")) {
+		std::cout << "Type not set.\n";
+	} else {
+		std::cout << "Type set to: " << ltype << ".\n";
+	}
+
+	return all_set;
+}
+
+int main(int argc, char* argv[]) { 
+	load_from_dict ldict = load_from_dict();
+	bool all_set = true;
+	
+	po::options_description desc("Allowed options");
+	try {
+
+		all_set = ldict.parse_args(argc, argv);
+	} catch(std::exception& e) {
+       	std::cerr << "error: " << e.what() << "\n";
+		ldict.print_help();
+       	return 1;
+   	}
+   	catch(...) {
+       	std::cerr << "Exception of unknown type!\n";
+		ldict.print_help();
+   	}
+
+	if (!all_set) {
+		ldict.print_help();
+		return 0;
+	}
+
+	std::string ltype = ldict.get_type();
+	if (ltype.compare("simple") == 0) {
+		ldict.build_tree_simple();
+	} else if (ltype.compare("complex") == 0) {
+		ldict.build_tree_complex();
+	} else {
+		std::cerr << "Invalid type: " << ltype << ".\n";
+	}
+	ldict.save_tree();
         
     return 0;
 }
